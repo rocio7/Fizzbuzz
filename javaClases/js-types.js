e255a3cfaf2a8fc570f955e0da3fefca4a1b1929
Rocio <rocioma324@gmail.com>
2022-02-18 17:03:27 -0500

    Primera practica en javascript

---
+++ javaClases/js-types.js
@@ -0,0 +1,68 @@
+/* a una variable se le puede dar variables numericas, verdadero o falso o*/
+var num =1;
+var text = "Esta es una variable texto";
+var bool= false; /*true or false*/
+
+var numeroCambiante;
+console.log(numeroCambiante);
+numeroCambiante =10;
+console.log(numeroCambiante);
+numeroCambiante =25;
+console.log(numeroCambiante);
+
+var nombre="Rocio"
+var mensaje="Mi nombre es:";
+var mensajeCompleto=mensaje+nombre;
+console.log(mensajeCompleto);
+
+var n=13;
+n += 3;
+console.log(n);
+
+var n =13;
+n *= 3;
+console.log(n);
+
+var nom ="Rocio"
+console.logo(nom);
+nom = true;
+console.log(nom);
+nom =1234;
+console.log(nom);
+
+var n=13;
+if(n>=15) {
+    console.log("Numero mayor a 15");
+}else if(n<=5) {
+    console.log("Num menor a 5");
+}else {
+    console.log("Numero entre 5 y 15");
+}
+var temperatura = 20;
+var estaLloviendo= true;
+
+
+
+
+
+var x=1;
+while(x<8){
+    console.log(X)
+}
+
+var comienzo=1;
+var terminar=5;
+while(comienzo <=terminar) {
+    console.log("Comienzo:"+comienzo+";Termino:"+terminar);
+    comienzo++;
+    terminar--;
+}
+var dia1 = "Lunes";
+var dia2 = "Martes";
+var dia3 = "Miercoles"
+var dia4 = "Jueves";
+var dia5 = "Viernes";
+
+var diasSemana = { "Lune",}
+
+
